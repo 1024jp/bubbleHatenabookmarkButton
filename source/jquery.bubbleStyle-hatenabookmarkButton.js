e0d3fcff4b785d302979d51b66b7b3d50dbcaabd
1024jp <1024jp@wolfrosch.com>
2012-07-24 08:32:16 +0200

    [mod] functionだったgetJSONを中に

--- source/jquery.bubbleStyle-hatenabookmarkButton.js
+++ source/jquery.bubbleStyle-hatenabookmarkButton.js
@@ -7,25 +7,20 @@
 $(function(){
 	$('a.hatena-bookmark-button[data-hatena-bookmark-layout="standard"]').attr('data-hatena-bookmark-layout','simple').addClass('bubble');
 
-	if (0 < $('.hatena-bookmark-button.bubble').size()) {
-		$('a.hatena-bookmark-button.bubble').each(function(){
-			// get entry url
-		 	var entryUrl;
-		 	entryUrl = $(this).attr('href').replace('http://b.hatena.ne.jp/entry/','');
-		 	if (!entryUrl) {
-		 		entryUrl = $(location).attr('href');
-		 	} else if (!entryUrl.match(/^http/)) {
-		 		entryUrl = 'http://' + entryUrl;
-		 	}
-		 	
-		 	// get count
-			getHatenaBookmarkCount(entryUrl, $(this));
+	$('a.hatena-bookmark-button.bubble').each(function(){
+		// get entry url
+	 	var entryUrl;
+	 	entryUrl = $(this).attr('href').replace('http://b.hatena.ne.jp/entry/','');
+	 	if (!entryUrl) {
+	 		entryUrl = $(location).attr('href');
+	 	} else if (!entryUrl.match(/^http/)) {
+	 		entryUrl = 'http://' + entryUrl;
+	 	}
+	 	
+	 	// get count
+		var placeholder = $(this);
+		$.getJSON('http://api.b.st-hatena.com/entry.count?url='+entryUrl+'&callback=?', function(count) {
+			placeholder.append('<span class="count">'  + count + '</span>');
 		});
-	}
-});
-
-function getHatenaBookmarkCount(entryUrl, placeholder) {
-	$.getJSON('http://api.b.st-hatena.com/entry.count?url='+entryUrl+'&callback=?', function(count) {
-		placeholder.append('<span class="count">'  + count + '</span>');
 	});
-}
+});
