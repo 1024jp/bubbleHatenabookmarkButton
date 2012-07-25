2287921a6202d1da65281edd8bf150799311c346
1024jp <1024jp@wolfrosch.com>
2012-07-25 02:01:57 +0200

    [mod] 実行タイミングの調整
    asyncとdefer付けて $(function(){})外した。
    ちょっと挙動に自信が無い。

--- source/jquery.bubbleHatenabookmarkButton.js
+++ source/jquery.bubbleHatenabookmarkButton.js
@@ -6,32 +6,30 @@
 
 
 (function($){
-	$(function(){
-// 		// append stylesheet (@@@ will be replaced to CSS code when this minified
-// 		$('head').append('<style>@@@</style>');
-	
-		// define hatena bookmark standard button
-		var standardButtons = $('a.hatena-bookmark-button[data-hatena-bookmark-layout="standard"]');
-	
-		// execute
-		standardButtons.each(function(){
-			// mimic simple button and give bubble class
-			$(this).attr('data-hatena-bookmark-layout','simple').addClass('bubble');
-			
-			// get entry url
-		 	var entryUrl;
-		 	entryUrl = $(this).attr('href').replace('http://b.hatena.ne.jp/entry/','');
-		 	if (!entryUrl) {
-		 		entryUrl = $(location).attr('href');
-		 	} else if (!entryUrl.match(/^http/)) {
-		 		entryUrl = 'http://' + entryUrl;
-		 	}
-		 	
-		 	// get count from Hatena via JSON
-			var placeholder = $(this);
-			$.getJSON('http://api.b.st-hatena.com/entry.count?url='+entryUrl+'&callback=?', function(count){
-				placeholder.append('<span class="count">' + count + '</span>');
-			});
+// 	// append stylesheet (@@@ will be replaced to CSS code when this minified
+// 	$('head').append('<style>@@@</style>');
+
+	// define hatena bookmark standard button
+	var standardButtons = $('a.hatena-bookmark-button[data-hatena-bookmark-layout="standard"]');
+
+	// execute
+	standardButtons.each(function(){
+		// mimic simple button and give bubble class
+		$(this).attr('data-hatena-bookmark-layout','simple').addClass('bubble');
+		
+		// get entry url
+	 	var entryUrl;
+	 	entryUrl = $(this).attr('href').replace('http://b.hatena.ne.jp/entry/','');
+	 	if (!entryUrl) {
+	 		entryUrl = $(location).attr('href');
+	 	} else if (!entryUrl.match(/^http/)) {
+	 		entryUrl = 'http://' + entryUrl;
+	 	}
+	 	
+	 	// get count from Hatena via JSON
+		var placeholder = $(this);
+		$.getJSON('http://api.b.st-hatena.com/entry.count?url='+entryUrl+'&callback=?', function(count){
+			placeholder.append('<span class="count">' + count + '</span>');
 		});
 	});
 })(jQuery);
